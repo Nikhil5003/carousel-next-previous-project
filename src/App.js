a5c9fe46c6506dd98c38bdff0a3f1d3b3be2ff86
Nikhil5003 <nikhil9812382543@gmail.com>
2023-11-29 23:42:45 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 function App() {
+  const [currentIndex, setIndex] = useState(0);
+  const images = [
+    "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+  const handleNext = () => {
+    setIndex((prev) => (prev + 1) % images.length);
+  };
+  const handlePrevious = () => {
+    setIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carousel-class">
+        <img
+          key={currentIndex}
+          src={images[currentIndex]}
+          loading="lazy"
+          alt=""
+        />
+        <div className="icon previousIcon" onClick={handlePrevious}>
+          <FaArrowLeft
+            style={{
+              height: "50px",
+              width: "40px",
+              color: "whiteSmoke",
+            }}
+          />
+        </div>
+        <div className=" icon nextIcon" onClick={handleNext}>
+          <FaArrowRight
+            style={{
+              height: "50px",
+              width: "40px",
+              color: "whiteSmoke",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
